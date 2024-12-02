01cd4f960cadf7db159fd5b44be1e2b5531672d7
Berk Erdoğan <erdgnberk@gmail.com>
2024-12-02 14:11:53 +0300

    App bar and bugs
    App bar and bugs were fixed

--- app/src/main/java/com/berkerdgn/e_commerce_mobil_app/view/auth_view/LoginFragment.kt
+++ app/src/main/java/com/berkerdgn/e_commerce_mobil_app/view/auth_view/LoginFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.berkerdgn.e_commerce_mobil_app.R
+import com.berkerdgn.e_commerce_mobil_app.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
+    private var _binding : FragmentLoginBinding ?= null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +25,28 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_secondaryMainFragment)
+        }
+
+        binding.signUpTextView4.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_singUpFragment)
+        }
+
+
+    }
+
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 
 
--- app/src/main/java/com/berkerdgn/e_commerce_mobil_app/view/home_view/SecondaryMainFragment.kt
+++ app/src/main/java/com/berkerdgn/e_commerce_mobil_app/view/home_view/SecondaryMainFragment.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.berkerdgn.e_commerce_mobil_app.R
+import com.berkerdgn.e_commerce_mobil_app.databinding.FragmentSecondaryMainBinding
 
 
 class SecondaryMainFragment : Fragment() {
 
+    private var _binding: FragmentSecondaryMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +24,37 @@ class SecondaryMainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_secondary_main, container, false)
+        _binding = FragmentSecondaryMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        replace(HomeFragment())
+
+        binding.navView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> replace(HomeFragment())
+                R.id.navigation_liked -> replace(LikedFragment())
+                R.id.navigation_user -> replace(HomeFragment())
+            }
+            true
+        }
+
+
+    }
+
+    private fun replace(fragment: Fragment) {
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.navHostFrameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
 
 }
